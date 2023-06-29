5e431baa44c6d364c9b2ae91e12f44bd82758d47
Eduard Knezovic <knezovic.eduard@gmail.com>
2023-06-29 11:16:39 +0200

    write code to respond to user with total amount of an activity

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,8 @@
 
+from typing import List
+
 import sqlite3
+
 from app.models.activity import Activity
 
 def add(activity: Activity):
@@ -29,6 +32,30 @@ def add(activity: Activity):
     # Return the ID of the inserted activity
     return activity_id
 
-def get_total_number_of_pushups(user_id: int) -> int:
-    activity_name = "pushups"
-    raise NotImplemented
+def get_all_user_activity(user_id: int) -> List[Activity]:
+    # Connect to the SQLite database
+    conn = sqlite3.connect('activities.db')
+    cursor = conn.cursor()
+
+    # Retrieve all activities for the given user_id
+    cursor.execute('SELECT id, name, timestamp, amount, user_id FROM activities WHERE user_id = ?', (user_id,))
+    result_set = cursor.fetchall()
+
+    # Process the retrieved data and create a list of Activity objects
+    activities = []
+    for row in result_set:
+        activity = Activity(
+            id=row[0],
+            name=row[1],
+            timestamp=row[2],
+            amount=row[3],
+            user_id=row[4]
+        )
+        activities.append(activity)
+
+    # Close the database connection
+    conn.close()
+
+    # Return the list of Activity objects
+    return activities
+
--- app/service.py
+++ app/service.py
@@ -1,13 +1,24 @@
 
-import random
+from typing import List
 
 from app.models.activity import Activity 
+from app.crud import get_all_user_activity
 
 def get_response_for_successful_updating_of_activity(activity: Activity) -> str:
     message = "+{} {}!".format(activity.amount, activity.name, activity.user_id)
     message += "\n\n"
+    message += "You've completed a total of {} {}. Keep going!".format(get_activity_sum(activity.user_id, activity.name), activity.name)
+    message += "\n\n"
     message += get_customized_message(activity)
     return message
+
+def get_activity_sum(user_id: int, activity_name: str) -> int:
+    activities: List[Activity] = get_all_user_activity(user_id)
+    total = 0
+    for activity in activities:
+        if activity.name == activity_name:
+            total += activity.amount
+    return total
 
 def get_customized_message(activity: Activity = None) -> str: # "activity" is an optional parameter
     messages = [
