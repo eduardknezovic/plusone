e613067a9eedce6b0ee5b881c1d28fecaca6faeb
Anton Antonov <antonantonov@Antons-MacBook-Pro.local>
2023-08-26 21:19:49 +0300

    26 August modification

--- nova/app.py
+++ nova/app.py
@@ -5,6 +5,7 @@ import base64
 from PIL import Image
 import io
 import pytesseract
+import re
 
 app = dash.Dash(__name__)
 
@@ -31,82 +32,85 @@ app.layout = html.Div([
     html.Div(id='output-image'),
 ])
 
+# Handling of exceptions with replacements
 def filter_extracted_text(extracted_text):
-    # Handle text recognition common errors
-    print("BEFORE FILTERING: ")
-    print(extracted_text)
-    extracted_text = extracted_text.replace("p", "")
-    extracted_text = extracted_text.replace("u", "")
-    extracted_text = extracted_text.replace("m", "")
-    extracted_text = extracted_text.replace("o", "0")
-    extracted_text = extracted_text.replace("O", "0")
-    extracted_text = extracted_text.replace("+0", "+0.")
-    extracted_text = extracted_text.replace("+0..", "+0.")
-    extracted_text = extracted_text.replace("-0", "-0.")
-    extracted_text = extracted_text.replace("-0..", "-0.")
-    extracted_text = extracted_text.replace("+1", "+1.")
-    extracted_text = extracted_text.replace("+1..", "+1.")
-    extracted_text = extracted_text.replace("-1", "-1.")
-    extracted_text = extracted_text.replace("-1..", "-1.")
-    extracted_text = extracted_text.replace("+2", "+2.")
-    extracted_text = extracted_text.replace("+2..", "+2.")
-    extracted_text = extracted_text.replace("-2", "-2.")
-    extracted_text = extracted_text.replace("-2..", "-2.")
-    extracted_text = extracted_text.replace("-2..", "-2.")
+    
+    # Needed replacements for correcting the text regognition errors
+    replacements = {
+    "p": "",
+    "u": "",
+    "m": "",
+    "o": "0",
+    "O": "0",
+    "‘": "",
+    "“": "",
+    r"\+(\d)": r"+\1.",     # Handle missing dot on plus
+    r"-(\d)": r"-\1.",      # Handle missing dot on minus
+    r"(\d)\.\.": r"\1.",    # Handle tow dots ..
+    r"\.(\.+)": r"\1",      # Handle any additinal dots
+    r"(\d+)\.(\d+)\.(\d+)": r"\1.\2\3",
+    r"(\d+)\.\.{2,}(\d+)": r"\1.\2"
+    }
 
-    extracted_text = extracted_text[:5]
+    # Additional replacement to keep only the first dot after the first character
+    replacements[r"(\d)([.]+)(\d+)"] = r"\1.\3"
+
+    # Execute the replacements
+    for pattern, replacement in replacements.items():
+        extracted_text = re.sub(pattern, replacement, extracted_text)
+
+    # Hande the occuring of additional char at end error
+    if len(extracted_text) > 5:
+        extracted_text = extracted_text[:-1]
     
     return extracted_text
 
 def extract_data(image):
 
-    crop_coordinates_list = [
-        (281,367,359,381),  # Crop 1 coordinates
-        (258,905,295,920),  # Crop 2 coordinates
-        (318,905,356,920),  # Crop 3 coordinates
-        (258,934,295,951),  # Crop 4 coordinates
-        (319,936,356,953),  # Crop 5 coordinates
-        (156,936,224,953),  # Crop 6 coordinates
-        (156,968,224,983),  # Crop 7 coordinates
-        (302,1104,357,1119) # Crop 8 coordinates
-    ]
+    # Dict for keys and coordinates
+    crop_coordinates_dict = {
+        "eye":                      (281,367,359,381),   # Crop 1  coordinates
+        "pupil_center_x":           (258,905,295,920),   # Crop 2  coordinates
+        "pupil_center_y":           (318,905,356,920),   # Crop 3  coordinates
+        "pachy_apex_x":             (258,934,295,951),   # Crop 4  coordinates
+        "pachy_apex_y":             (319,936,356,953),   # Crop 5  coordinates
+        "pachy_apex_thickness":     (156,936,224,953),   # Crop 6  coordinates
+        "thinnest_local_thickness": (156,968,224,983),   # Crop 7  coordinates
+        "pupil_diameter":           (302,1104,357,1119), # Crop 8  coordinates
+        "k_max_x":                  (259,999,295,1015),  # Crop 9  coordinates
+        "k_max_y":                  (319,999,357,1014)  # Crop 10 coordinates
+    }
 
+    # dict to store the data extracted
     extracted_data = {}
 
-    keys = [
-        "eye:",
-        "pupil_center_x:",
-        "pupil_center_y:",
-        "pachy_apex_x:",
-        "pachy_apex_y:",
-        "pachy_apex_thickness:",
-        "thinnest_local_thickness:",
-        "pupil_diameter:"
-    ]
-
-    for idx, crop_coordinates in enumerate(crop_coordinates_list):
+    for idx, crop_coordinates in enumerate(crop_coordinates_dict.values()):
         cropped_image = image.crop(crop_coordinates)
         
         # Perform OCR on the cropped image region
         extracted_text = pytesseract.image_to_string(cropped_image, config='--psm 6').strip()
-
         
         # Handle missing - sing
-        if idx == 1 or idx == 2:
-            if '+' not in extracted_text:
-                extracted_text = '-' + extracted_text
+        if idx == 1 or idx == 2 or idx == 8 or idx == 9:
+            if '-' in extracted_text:
+                extracted_text = extracted_text
+            elif '+' not in extracted_text:
+                if extracted_text == 0.00 or 0.0:
+                    extracted_text = extracted_text
+                else:
+                    extracted_text = '-' + extracted_text
 
         extracted_text = filter_extracted_text(extracted_text)
 
-        # Use the key to label the values and make them the right type
+        # Convert to right type
+        key_at_idx = list(crop_coordinates_dict.keys())[idx]
         if idx == 0:
-            extracted_data[idx + 1] = f"{keys[idx]} {extracted_text}"
+            extracted_value = extracted_text
         elif idx == 5 or idx == 6:
             extracted_value = int(extracted_text)
-            extracted_data[idx + 1] = f"{keys[idx]} {extracted_value}"
         else:
-            extracted_value2 = float(extracted_text)
-            extracted_data[idx + 1] = f"{keys[idx]} {extracted_value2:.2f}"
+            extracted_value = float(extracted_text)
+        extracted_data[key_at_idx] = extracted_value
 
     return extracted_data
 
